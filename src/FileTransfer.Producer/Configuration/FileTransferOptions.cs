c74e086e26cccdd4e1aaaee9bb30ea6da905fc39
Aleksandar Stojanovski <astojanovski88@gmail.com>
2026-09-19 21:00:08 +0200

    Dependency injection setup + project configuration

---
+++ src/FileTransfer.Producer/Configuration/FileTransferOptions.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FileTransfer.Producer.Configuration
+{
+    public class FileTransferOptions
+    {
+        public int ChunkSize { get; set; } = 1024;
+    }
+}
--- src/FileTransfer.Producer/Program.cs
+++ src/FileTransfer.Producer/Program.cs
@@ -1,1 +1,23 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using FileTransfer.Infrastructure;
+using FileTransfer.Producer.Configuration;
+using FileTransfer.Producer.Interfaces;
+using FileTransfer.Producer.Services;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var builder = Host.CreateApplicationBuilder(args);
+        builder.Services.Configure<FileTransferOptions>(builder.Configuration.GetSection("FileTransferOptions"));
+        builder.Services.AddSingleton<IChunkTransport, InMemoryChunkTransport>();
+        builder.Services.AddSingleton<IFileChunkerService, FileChunkerService>();
+        builder.Services.AddSingleton<IFileTransferSenderService, FileTransferSenderService>();
+        using var host = builder.Build();
+
+        var fileTransferSender = host.Services.GetRequiredService<IFileTransferSenderService>();
+        fileTransferSender.SendFile("Data/sample-10mb.jpg").GetAwaiter().GetResult();  
+    }
+}
