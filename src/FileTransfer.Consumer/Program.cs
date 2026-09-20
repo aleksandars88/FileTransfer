1641472b8fce8de141e073845d59395895fdca61
Aleksandar Stojanovski <astojanovski88@gmail.com>
2026-09-20 19:49:57 +0200

    Main setup and dependency injection into consumer app

--- src/FileTransfer.Consumer/Program.cs
+++ src/FileTransfer.Consumer/Program.cs
@@ -1,1 +1,37 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using FileTransfer.Consumer.Configuration;
+using FileTransfer.Consumer.Interfaces;
+using FileTransfer.Consumer.Services;
+using FileTransfer.Infrastructure.Storage;
+using FileTransfer.Infrastructure.Transport;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Options;
+
+internal class Program
+{
+    private async static Task Main(string[] args)
+    {
+        var builder = Host.CreateApplicationBuilder(args);
+
+        builder.Services.Configure<FileTransferConsumerOptions>(builder.Configuration.GetSection("FileTransfer"));
+
+        builder.Services.AddSingleton<IFileChunkStorage>(sp => {
+            var options = sp.GetRequiredService<IOptions<FileTransferConsumerOptions>>().Value;
+            return new FileChunkStorage(options.StorageDirectory);
+        });
+
+        builder.Services.AddSingleton<IFileChunkAssembler, FileChunkAssembler>();
+
+        builder.Services.AddSingleton<IFileTransferReceiver, FileTransferReceiver>();
+
+        builder.Services.AddSingleton<IChunkTransport, InMemoryChunkTransport>();
+
+        var host = builder.Build();
+
+        var receiver = host.Services.GetRequiredService<IFileTransferReceiver>();
+
+        await receiver.ReceiveFileChunks();
+
+    }
+}
